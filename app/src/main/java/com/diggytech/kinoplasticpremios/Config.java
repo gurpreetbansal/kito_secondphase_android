5d2180625d824652f744f1fee774444b5b13f793
HP <gaurav@imarkinfotech.com>
2020-05-13 10:35:53 +0530

    13/05/2020

--- app/src/main/java/com/diggytech/kinoplasticpremios/Config.java
+++ app/src/main/java/com/diggytech/kinoplasticpremios/Config.java
@@ -8,7 +8,9 @@ public class Config {
     //public  static  final String BASE_URL= "https://clientstagingdev.com/kinoplastic_server/api/";
     //public  static  final String BASE_URL= "https://diggy.tech/api/";
 //    public  static  final String BASE_URL= "http://68.183.74.38:8008/joao/api/";
-    public  static  final String BASE_URL= "https://clientstagingdev.com/joao/api/";
+//    public  static  final String BASE_URL= "https://clientstagingdev.com/joao/api/";
+
+    public  static  final String BASE_URL= "https://joao.customer-devreview.com/api";
 // val BASE_URL: String = "http://68.183.74.38:8008/joao/api/"
 
 
